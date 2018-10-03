1b5561799dbdb1f24d1531be9433c1db587a0ba3
Christina Clark <divackc@gmail.com>
2018-10-03 23:00:34 +0000

    second push

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -6,21 +6,102 @@ puts "Please enter freezing temperature in fahrenheit:"
  temperature = gets.to_f
   temperature == "32" 
   celsius = (temperature - 32) * 5 / 9
+  if temperature == "32"
   puts "Temperature in celsius: #{celsius}"
+ else 
+puts "Please enter freezing temperature in fahrenheit:"
+puts "Hint: 32"
+temperature = gets.to_f
+  temperature == "32" 
+  celsius = (temperature - 32) * 5 / 9
+  puts "Temperature in celsius: #{celsius}"
+ end
 puts " "
-  puts "Please enter boiling temperature in fahrenheit:"
+puts "Please enter boiling temperature in fahrenheit:"
  temperature = gets.to_f
+  temperature == "32" 
+  celsius = (temperature - 32) * 5 / 9
+  if temperature == "32"
+  puts "Temperature in celsius: #{celsius}"
+ else 
+puts "Please enter boiling temperature in fahrenheit:"
+puts "Hint: 212"
+temperature = gets.to_f
   temperature == "212" 
   celsius = (temperature - 32) * 5 / 9
   puts "Temperature in celsius: #{celsius}"
+ end
 puts " "
-  puts "Please enter body temperature in fahrenheit:"
+puts "Please enter body temperature in fahrenheit:"
  temperature = gets.to_f
   temperature == "98.6" 
   celsius = (temperature - 32) * 5 / 9
+  if temperature == "32"
   puts "Temperature in celsius: #{celsius}"
+ else 
+puts "Please enter body temperature in fahrenheit:"
+puts "Hint: 98.6"
+temperature = gets.to_f
+  temperature == "98.6" 
+  celsius = (temperature - 32) * 5 / 9
+  puts "Temperature in celsius: #{celsius}"
+ end
 puts " "
-  puts "Please enter a random temperature in fahrenheit:"
+ puts "Please enter random temperature in fahrenheit:"
  temperature = gets.to_f
   celsius = (temperature - 32) * 5 / 9
   puts "Temperature in celsius: #{celsius}"
+puts " "
+puts "Now let\'s convert back to fahrenheit!"
+puts " "
+puts "Please enter freezing temperature in celsius:"
+ temperature = gets.to_f
+  temperature == "0" 
+  fahrenheit = (temperature * 1.8) + 32
+  if temperature == "0"
+  puts "Temperature in celsius: #{fahrenheit}"
+ else 
+puts "Please enter freezing temperature in celsius:"
+puts "Hint: 0"
+temperature = gets.to_f
+  temperature == "0" 
+  fahrenheit = (temperature * 1.8) + 32
+  puts "Temperature in fahrenheit: #{fahrenheit}"
+ end
+ puts "Please enter boiling temperature in celsius:"
+ temperature = gets.to_f
+  temperature == "100" 
+  fahrenheit = (temperature * 1.8) + 32
+  if temperature == "100"
+  puts "Temperature in celsius: #{fahrenheit}"
+ else 
+puts "Please enter boiling temperature in celsius:"
+puts "Hint: 100"
+temperature = gets.to_f
+  temperature == "100" 
+  fahrenheit = (temperature * 1.8) + 32
+  puts "Temperature in fahrenheit: #{fahrenheit}"
+ end
+ puts " "
+ puts "Please enter body temperature in celsius:"
+ temperature = gets.to_f
+  temperature == "37" 
+  fahrenheit = (temperature * 1.8) + 32
+  if temperature == "37"
+  puts "Temperature in celsius: #{fahrenheit}"
+ else 
+puts "Please enter body temperature in celsius:"
+puts "Hint: 37"
+temperature = gets.to_f
+  temperature == "37" 
+  fahrenheit = (temperature * 1.8) + 32
+  puts "Temperature in fahrenheit: #{fahrenheit}"
+ end
+ puts "Please enter a random temperature in celsius:"
+ temperature = gets.to_f
+  temperature == "0" 
+  fahrenheit = (temperature * 1.8) + 32
+  puts "Temperature in celsius: #{fahrenheit}"
+ puts " "
+ puts "Thank you for using my temperature converter!"
+ puts "Goodbye!!!"
