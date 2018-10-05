db8da36de33113eb52bcd3e1e9115fa3fca229d7
Christina Clark <divackc@gmail.com>
2018-10-05 15:46:13 +0000

    final calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,18 @@
-#write your code here
+ def add_three(number)
+     number + 3
+ end
+ 
+ puts add_three(4)
+ 
+ def subtract_two(number)
+  number - 2
+ end
+
+ puts subtract_two(7)
+ 
+ sum = 0
+[1, 2, 3].each do |i|
+  sum += i
+end
+
+puts sum  
