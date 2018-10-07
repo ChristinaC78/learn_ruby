8cee3d1f32d707b12ecf1fede67bf5af83dd4912
Christina Clark <divackc@gmail.com>
2018-10-07 01:33:18 +0000

    finished

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,13 @@
-#write your code here
+puts "Do you hear an echo?"
+puts "hint: Ask nicely and i'll STOP"
+echo = gets.chomp
+array = Array.new
+
+while echo != "STOP"
+  
+  array << gets.chomp
+  echo = array.last
+  
+end
+puts " "
+puts array
