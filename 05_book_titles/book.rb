14ceab539bcc3a8a476616474c315655b7d1a387
Christina Clark <divackc@gmail.com>
2018-10-07 00:45:21 +0000

    finished

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
-class Book
-# write your code here
+  title = ("inferno")
+    puts title.capitalize
+
+  title2 = ("stuart little")
+   puts title2.split(" ").map {|w|w.capitalize}.join(" ")
+   
+  Little = %w[the to a an in of and]
+def titleize s
+  s.gsub(/\w+/)
+  .with_index{|w, i| i.zero? || Little.include?(w).! ? w.capitalize : w}
 end
+
+puts titleize( "to kill a mockingbird")
+puts titleize( "alexander the great")
+puts titleize( "to eat an apple a day")
+puts titleize( "war and peace")
+
+  title2 = ("what i wish i knew when i was 20")
+   puts title2.split(" ").map {|w|w.capitalize}.join(" ")
